8f3c3b0f0deed31f545e66083a67fc8665ecb4d9
Marcus Crafter <crafterm@redartisan.com>
2010-11-18 23:07:20 +1100

    [NEW] Package header file

---
+++ MRCEnumerable.h
@@ -0,0 +1,13 @@
+//
+//  MRCEnumerable.h
+//  MRCEnumerable
+//
+//  Created by Marcus Crafter on 18/11/10.
+//  Copyright 2010 Red Artisan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "NSArray+Utilities.h"
+#import "NSArray+Enumerable.h"
+#import "NSDictionary+Enumerable.h"
